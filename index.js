c41d7f53a81f106f5391f307f1711dad027de8c3
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-11-10 09:38:22 -0500

    commit tree cbefe781de6f036fe00ca59291419ae918fc9227

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+const findMatching = (a,b) => a.filter(a => a.toLowerCase() === b.toLowerCase())
+
+const fuzzyMatch = (a,b) => a.filter(a => a.charAt(0) === b.charAt(0))
+
+
+const matchName = (a,b) => a.filter(a => a.name === b)
